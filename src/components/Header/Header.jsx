611463a0414cb235e08043f530e0bdb7b8065297
savidyainduma <Savidyainduma@gmail.com>
2024-10-29 15:21:04 +0530

    header completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import './Header.css'
 
+import {
+  BsJustify,
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+} from "react-icons/bs";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <div className="menu-icon">
+        <BsJustify className="icon" />
+      </div>
+      <div className="header-left">
+        <BsSearch className="icon" />
+      </div>
+      <div className="header-right">
+        <BsFillBellFill className="icon"/>
+        <BsFillEnvelopeFill className="icon"/>
+        <BsPersonCircle className="icon"/>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
